44503fca9ae9f765fc3605ea63ab99fa3be4eec0
Marie5646 <125408068+Marie5646@users.noreply.github.com>
2024-09-15 22:26:41 -0400

    20.11

--- modules/src/ChapterTwenty/MatchGroupSymbols.java
+++ modules/src/ChapterTwenty/MatchGroupSymbols.java
@@ -1,5 +1,6 @@
 package ChapterTwenty;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class MatchGroupSymbols {
@@ -45,8 +46,10 @@ public class MatchGroupSymbols {
 
 
         public static void main (String[]args){
-            String myStr = "{[}}";
-            String checkIfMatch = isMatching(myStr) ? "MATCHING" : "NOT MATCHING";
+            Scanner myScan = new Scanner(System.in);
+            System.out.println("Enter a string that includes symbols such as [,{ ,( ,) ,},]: ");
+            String userInput = myScan.nextLine();
+            String checkIfMatch = isMatching(userInput) ? "MATCHING" : "NOT MATCHING";
             System.out.println(checkIfMatch);
 
         }
@@ -64,7 +67,6 @@ public class MatchGroupSymbols {
 
 
 
-//    (COUNT THE KEYWORDS IN JAVA SOURCE CODE) Revise the program in Listing 21.7. If a keyword is in a comment or in a string, don’t count it. Pass the Java file name from the command line. Assume the Java source code is correct and line comments and paragraph comments do not overlap.
 
 
 
@@ -82,20 +84,6 @@ public class MatchGroupSymbols {
 
 
 
-
-//    (IMPLEMENT A DOUBLY LINKED LIST) The MyLinkedList class used in Listing 24.5 is a one-way directional linked list that enables one-way traversal of the list. Modify the Node class to add the new data field name previous to refer to the previous node in the list, as follows:
-//
-//
-//    public class Node<E> {
-//        E element;
-//        Node<E> next;
-//        Node<E> previous;
-//
-//        public Node(E e) {
-//            element = e;
-//        }
-//    }
-//    Implement a new class named TwoWayLinkedList that uses a doubly linked list to store elements. Define TwoWayLinkedList to implements MyList. You need to implement all the methods defined in MyLinkedList as well as the methods listIterator() and listIterator(int index). Both return an instance of java.util.ListIterator<E> (see Figure 20.4). The former sets the cursor to the head of the list and the latter to the element at the specified index. Test your new class using this code from https://liveexample.pearsoncmg.com/test/Exercise24_03.txt.
 
 
 
